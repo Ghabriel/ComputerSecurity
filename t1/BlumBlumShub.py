1d1ffb424790c8d953e817440226890540f6ec73
Ghabriel <ghabriel.nunes@gmail.com>
2017-04-06 20:12:04 -0300

    Bugfixes and documentation improvements

--- t1/BlumBlumShub.py
+++ t1/BlumBlumShub.py
@@ -9,20 +9,25 @@ class BlumBlumShub:
 		p = (2 ** 2203) - 1
 		q = (2 ** 4253) - 1
 
+		# Calcula o parâmetro M do algoritmo.
 		self.m = p * q
 
+		# Evita que a semente assuma os valores 0 ou 1.
 		seed = max(2, seed)
 
 		# Garante que o valor da semente seja co-primo a M.
-		if seed % 2 != 1:
+		if seed % 2 == 1:
 			seed += 1
 
+		# Armazena a semente inicial e o tamanho desejado.
 		self.seed = seed
-
 		self.size = size
 
 	# Gera um novo valor aleatório e o retorna.
 	def generate(self):
 		self.seed = (self.seed * self.seed) % self.m
-		bin_value = bin(self.seed)
-		return int(str(bin_value)[-self.size:], 2)
+		bin_value = bin(self.seed)[2:]
+		# Obtém os N bits menos significativos da nova semente,
+		# onde N é o tamanho desejado, e retorna sua
+		# representação decimal.
+		return int(bin_value[-self.size:], 2)
